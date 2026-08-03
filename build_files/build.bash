90c82d078e8e2998f2d8f9e0aa856b7943d41c61
lina-bh <36717206+lina-bh@users.noreply.github.com>
2026-08-03 16:56:45 +0100

    change package removal strategy
    Mark unwanted packages as dependencies and autoremove them, rather than outright
    deleting them. This lessens the possibility that we might delete something that
    we wanted installed. Since we know exactly what is in DNF's user installed list,
    the package set minimiser is no longer necessary, allowing us to delete 100
    lines of Clod code.
    
    Rule for removing packages from the packages_removed list:
    
    * If they are obviously necessary.
    * If I've ever used one of the binaries in it.
    * If it comes in the base image for i686 - it's probably going to be some form
    of free dependency for external binaries.

--- build_files/build.bash
+++ build_files/build.bash
@@ -1,65 +1,20 @@
 #!/bin/bash
 set -euxo pipefail
 
-dnf -y mark user \
-    ncurses-term \
-    terra-gamescope \
-    ImageMagick \
-    libjpeg-turbo-devel \
-    libpng-devel \
-    libwebp-devel \
-    perl-sigtrap \
-    setroubleshoot-server
+mapfile -t installed <<< "$(grep -Ev '^[[:space:]]*(#|$)' /ctx/packages_installed)"
 
-dnf -y --setopt=keepcache=True install python3-libdnf5
+dnf -y --setopt=keepcache=True install "${installed[@]}"
 
-if [[ ! -z "${MINIMISE-}" ]]; then
-  tmpfile="$(mktemp)"
-  python3 /ctx/minimise_remove.py < /ctx/packages_removed > "$tmpfile"
-  diff -u /ctx/packages_removed "$tmpfile" || :
-  mapfile -t pkgset < "$tmpfile"
-  rm "$tmpfile"
-else
-  set +x
-  full="$(grep -Ev '^[[:space:]]*(#|$)' /ctx/packages_removed)"
-  eval "pkgset=( ${full} )"
-  # shellcheck disable=SC2016
-  echo 'pkgset=( ${full} )'
-  set -x
-fi
-dnf -y remove "${pkgset[@]}"
+dnf -y --setopt=terra.enabled=1 --setopt=keepcache=True install ghostty
 
-dnf -y --setopt=keepcache=True install \
-    mpv \
-    nix nix-daemon \
-    autoconf \
-    dbus-devel \
-    giflib-devel \
-    gmp-devel \
-    gnutls-devel \
-    gtk3-devel \
-    gtk3-devel \
-    ImageMagick-devel \
-    libacl-devel \
-    libgccjit-devel \
-    libjpeg-turbo-devel \
-    libotf-devel \
-    librsvg2-devel \
-    libselinux-devel \
-    libtree-sitter-devel \
-    libwebp-devel \
-    libxml2-devel \
-    m17n-lib-devel \
-    ncurses-devel \
-    sqlite-devel \
-    systemd-devel \
-    texinfo \
-    info \
-    'perl(sigtrap)' \
-    zsh
+mapfile -t removed <<< "$(grep -Ev '^[[:space:]]*(#|$)' /ctx/packages_removed)"
+
+dnf -y mark dependency "${removed[@]}"
+dnf -y mark user "${installed[@]}" ghostty
+dnf -y \
+    --setopt=protected_packages=dnf5,glob:/etc/dnf/protected.d/*.conf,"$(IFS=,; echo "${installed[*]}")" \
+    autoremove
 
 # nix
 mv /nix /var/nix
 mkdir /nix
-
-dnf -y --setopt=terra.enabled=1 --setopt=keepcache=True install ghostty
