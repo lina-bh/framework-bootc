d1efd3ad1214f6456d8fe1dd007828147115821c
lina-bh <36717206+lina-bh@users.noreply.github.com>
2026-09-03 23:43:46 +0100

    further trimmage
    * certain packages particularly firmware refuse to go because they are weak deps
    of e.g linux-firmware. remove them explicitly.
    * try to replace glibc-all-langpacks with glibc-langpack-en

--- build_files/build.bash
+++ build_files/build.bash
@@ -5,20 +5,24 @@ dnf -y --setopt=keepcache=True swap wget2-wget wget1-wget
 
 mapfile -t installed <<< "$(grep -Ev '^[[:space:]]*(#|$)' /ctx/packages_installed)"
 
-dnf -y --setopt=keepcache=True install "${installed[@]}"
+dnf -y --no-best --setopt=keepcache=True install "${installed[@]}"
 
 dnf -y --setopt=terra.enabled=1 --setopt=keepcache=True install ghostty man-pages-posix
 
-mapfile -t removed <<< "$(grep -Ev '^[[:space:]]*(#|$)' /ctx/packages_removed)"
+mapfile -t unmarked <<< "$(grep -Ev '^[[:space:]]*(#|$)' /ctx/packages_unmarked)"
 
-dnf -y mark --skip-unavailable dependency "${removed[@]}"
+dnf -y mark --skip-unavailable dependency "${unmarked[@]}"
 dnf -y mark user "${installed[@]}" ghostty
 dnf -y \
     --setopt=protected_packages=dnf5,glob:/etc/dnf/protected.d/*.conf,"$(IFS=,; echo "${installed[*]}")" \
     autoremove
 
+mapfile -t removed <<< "$(grep -Ev '^[[:space:]]*(#|$)' /ctx/packages_removed)"
+
+dnf -y \
+    --setopt=protected_packages=dnf5,glob:/etc/dnf/protected.d/*.conf,"$(IFS=,; echo "${installed[*]}")" \
+    remove "${removed[@]}"
+
 # nix
 mv /nix /var/nix
 mkdir /nix
-
-rm -r /var/lib/openvpn || :
