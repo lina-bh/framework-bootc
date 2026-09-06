a0f17dd64de7f50b8876b2f147e20d82e88ddc90
lina-bh <36717206+lina-bh@users.noreply.github.com>
2026-09-06 22:45:21 +0100

    do not inherit labels from base image

--- rechunk.sh
+++ rechunk.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 set -eux
 : "${NAME:=ghcr.io/lina-bh/framework-bootc}"
+: "${SOURCE:=https://github.com/lina-bh/framework-bootc}"
 rechunked="${PWD}/rechunked"
 rm -rf "$rechunked"
 mkdir -p "$rechunked"
-set +x
-CHUNKAH_CONFIG_STR="$(podman inspect "${NAME}")"
-set -x
-export CHUNKAH_CONFIG_STR
-time podman run \
+podman run \
        --rm \
        --mount=type=image,source="${NAME}:latest",destination=/chunkah \
        --mount=type=bind,source="$rechunked",destination=/rechunked \
        --security-opt=label=disable \
-       --env=CHUNKAH_CONFIG_STR \
        quay.io/coreos/chunkah:latest build \
        --verbose \
        --max-layers=128 \
        --prune=/sysroot/ \
-       --label=ostree.commit- \
-       --label=ostree.final-diffid- \
+       --label=containers.bootc=1 \
+       --label=ostree.bootable=true \
+       --label=ostree.linux="$(podman run --rm "${NAME}" rpm -q --qf "%{VERSION}-%{RELEASE}.%{ARCH}\n" kernel-core)" \
+       --label=org.opencontainers.image.version="$(git log -n1 --oneline --no-decorate)" \
+       --label=org.opencontainers.image.revision="$(git rev-parse HEAD)" \
+       --label=org.opencontainers.image.source="$SOURCE" \
        --tag="${NAME}:latest" \
        --output=oci:/rechunked/out
